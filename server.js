0a3c829d86e40f581698ed9f64933f63755e8434
Manya Devadiga <manyadev2007@gmail.com>
2026-06-08 23:37:02 +0530

    Update server port for deployment

--- server.js
+++ server.js
@@ -91,6 +91,8 @@ app.get("/registrations", (req, res) => {
         }
     });
 });
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
